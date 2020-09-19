6283cda9dcb08f979c333ccbb5ad9d35848ed7e2
Johan Cañas <jxexcxo@gmail.com>
2020-09-18 21:56:47 -0500

    Colocando estilos al PokemonCard

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -8,6 +8,14 @@ const Sprite = styled.img `
 
     `;
 
+const Card = styled.div`
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    &:hover {
+        box-shadow: 0 14px 24px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    }
+`;
+
 class PokemonCard extends Component {
 
     
@@ -34,7 +42,7 @@ class PokemonCard extends Component {
 
     return(
         <div className="col-md-3 col-sm-6 mb-5">
-            <div className="card">
+            <Card className="card">
                 <h5 className="card-header">
                     {this.state.pokemonIndex}
                 </h5>
@@ -53,7 +61,7 @@ class PokemonCard extends Component {
                         }
                     </h6>
                 </div>
-            </div>
+            </Card>
 
         </div>
     )
