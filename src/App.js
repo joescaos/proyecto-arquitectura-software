7ce9a3d7e68be640a0101464018ddfb0076c984a
Johan Cañas <jxexcxo@gmail.com>
2020-09-19 20:20:39 -0500

    imagen de fondo para app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/layout/NavBar.js'
 import Dashboard from './components/layout/Dashboard';
+import BackgroundImage from './pattern.png';
 
 class App extends Component {
   render (){
     return(
-      <div className="App">
+      <div className="App" style={{ background: `url(${BackgroundImage})` }}>
         <Navbar />
         <div className="container">
           <Dashboard />
